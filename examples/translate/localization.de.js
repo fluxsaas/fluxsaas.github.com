9e2970ecbd0725d9d0df0f2db59c665c696674eb
kalle saas <kalle@kalle.badabum.local>
2010-01-29 13:12:54 +0100

    new post

---
+++ examples/translate/localization.de.js
@@ -0,0 +1,6 @@
+var LOCALIZATION = {
+  LANGUAGE: 'GERMAN',
+  WARNING: 'Achtung vor dem %s, er ist gefährlich !',
+  LOADING: 'Bitte warten, Daten werden geladen',
+  WELCOME: 'Hallo %s, wie geht\'s dir ?'
+};
